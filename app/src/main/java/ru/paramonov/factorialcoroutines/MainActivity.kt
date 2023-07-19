4b30378c301313d7970eae56f2ec708727a7973e
nikita <paramonov20@bk.ru>
2023-07-19 14:43:07 +0300

    Сделал простой макет, также написал основую логику для приложения, чтобы дальше изучать корутины

--- app/src/main/java/ru/paramonov/factorialcoroutines/MainActivity.kt
+++ app/src/main/java/ru/paramonov/factorialcoroutines/MainActivity.kt
@@ -2,10 +2,52 @@ package ru.paramonov.factorialcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import ru.paramonov.factorialcoroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.edTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+
+        viewModel.error.observe(this) { error ->
+            if(error) {
+                Toast.makeText(
+                    this,
+                    "Вы не ввели никое число!!!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        viewModel.progress.observe(this) { progress ->
+            if(progress) {
+                binding.progressCircular.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressCircular.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.factorial.observe(this) { result ->
+            binding.textViewResult.text = result
+        }
     }
 }
---
+++ app/src/main/java/ru/paramonov/factorialcoroutines/MainViewModel.kt
@@ -0,0 +1,37 @@
+package ru.paramonov.factorialcoroutines
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _error = MutableLiveData<Boolean>()
+    val error: LiveData<Boolean> = _error
+
+    private val _progress = MutableLiveData<Boolean>()
+    val progress: LiveData<Boolean> = _progress
+
+    private val _factorial = MutableLiveData<String>()
+    val factorial: LiveData<String> = _factorial
+
+    fun calculate(value: String?) {
+
+        _progress.value = true
+        if (value.isNullOrBlank()) {
+            _progress.value = false
+            _error.value = true
+            return
+        }
+
+        viewModelScope.launch {
+            val number = value.toLong()
+            delay(1000)
+            _progress.value = false
+            _factorial.value = number.toString()
+        }
+    }
+}
